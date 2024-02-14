ef6e4036a9f8fe7ce523634765e26286477c57d1
Bryansb23 <160052619+Bryansb23@users.noreply.github.com>
2024-02-14 11:48:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal🥣 ')
 streamlit.text('Kale, Spinach & Rocket Smoothie🥗')
 streamlit.text('Hard-Boiled Free-Range Egg🐔')
 streamlit.text('Avocado Toast🥑🍞')
+
+streamlit.header '🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

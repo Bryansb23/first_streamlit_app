5e2e4e8180125382af0d0c73c07ccf4b6bd32aa4
Bryansb23 <160052619+Bryansb23@users.noreply.github.com>
2024-02-14 15:48:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,7 @@ streamlit.dataframe (my_fruit_list)
 
 streamlit.multiselect ("Pick some fruits:", list (my_fruit_list.index), ['Avocado','Strawberries'])
 
+fruits_to_show = my_fruit_list.loc[fruist_selected]
+
+
 
